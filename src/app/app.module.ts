0bed003dce4beb5bd30add5f650e02b6921e943f
=papaya26 <juddisgood999@gmail.com>
2017-02-26 16:43:18 +0800

    implemted rounded inputs scss. modified behavior for not dirty inputs. fixed change password form validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { components } from './components';
 import { pages } from './pages';
 import { RootNavController } from '../services/services';
 import { CanvasWhiteboardModule } from 'ng2-canvas-whiteboard';
+import { EqualValidatorDirective } from '../shared/equal-validation.directive';
+
 @NgModule({
   declarations: [
     MyApp,
     pages,
-    components
+    components,
+    EqualValidatorDirective
   ],
   imports: [
     IonicModule.forRoot(MyApp),
-    CanvasWhiteboardModule
+    FormsModule,
+    ReactiveFormsModule,
+    CanvasWhiteboardModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
