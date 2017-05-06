9c76ea78405d320f13ad93a4400c3f6bdfc62067
papaya26 <juddisgood999@gmail.com>
2017-05-07 04:04:08 +0800

    refactored manager/tabs page. implemented alert for logout

--- src/pages/manager/manager.page.ts
+++ src/pages/manager/manager.page.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { App } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { App, AlertController } from 'ionic-angular';
 
 import { LoginPage } from '../login/login.page';
 import { TabsPage } from '../tabs/tabs';
@@ -12,29 +12,47 @@ import { RootNavController, Storage } from '../../services/services';
 	templateUrl: 'manager.html',
 	providers: [ManagerService]
 })
-export class ManagerPage {
+export class ManagerPage implements OnInit {
 
-	private clinicList: any[];
+	private clinics: any[];
 
-	constructor(private app: App,
-		private service: ManagerService,
+	constructor(
+		private app: App,
+		private alertController: AlertController,
+		private managerService: ManagerService,
 		private root: RootNavController,
 		private storage: Storage) {
+		this.clinics = [];
+	}
 
-		service.getClinicRecordByUserId().subscribe(response => {
+	public ngOnInit() {
+		this.managerService.getClinicRecordByUserId().subscribe(response => {
 			if (response && response.status) {
-				this.clinicList = response.result;
+				this.clinics = response.result;
 			}
 		});
 	}
 
-	public go(item) {
-		this.root.loadInit(item.clinicId);
+	public go(clinic) {
+		this.root.loadInit(clinic.clinicId);
 		this.app.getRootNav().setRoot(TabsPage);
 	}
 
 	public logout() {
-		this.storage.clear();
-		this.root.setRoot(LoginPage);
+		this.alertController.create({
+			message: `Are you sure you want to logout?`,
+			buttons: [
+				{
+					text: 'NO',
+					role: 'cancel',
+				},
+				{
+					text: 'YES',
+					handler: () => {
+						this.storage.clear();
+						this.app.getRootNav().setRoot(LoginPage);
+					}
+				}]
+		}).present();
 	}
 }	
