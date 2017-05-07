da611bc4db880a8f5d1031a45edce5136a89ae22
papaya26 <juddisgood999@gmail.com>
2017-05-08 03:27:37 +0800

    fixed error handling. fixed associate and delete member behavior. fixed behavior of search modal user.

--- src/components/search-user-modal/search-user-modal.ts
+++ src/components/search-user-modal/search-user-modal.ts
@@ -65,7 +65,7 @@ export class SearchUserModal implements OnInit {
 
     public selectUser(user) {
         this.alertController.create({
-            message: `${this.message} ${this.getFullName(user)}?`,
+            message: `${this.message} ${user.lastname ? this.getFullName(user) : user.email}?`,
             buttons: [
                 {
                     text: 'NO',
@@ -82,8 +82,7 @@ export class SearchUserModal implements OnInit {
     }
 
     public getFullName(user) {
-        return (user.lastname ? user.lastname + ', ' : '') + user.firstname + ' ' + (user.middlename ? user.middlename : '');
-    }
+        return (user.lastname ? user.lastname + ', ' : '') + (user.firstname ? user.firstname + ' ' : '') + ' ' + (user.middlename ? user.middlename : '');    }
 
     public displayContacts(userContacts) {
         if (userContacts && userContacts.length > 0) {
