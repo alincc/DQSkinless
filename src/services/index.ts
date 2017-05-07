7f4fbd379b958f98ba914a350e1418e08b1e3336
arsy <arseniosarmiento@gmail.com>
2017-05-07 21:06:54 +0800

    update icon with rounded corners; added loading screen after splashscreen

---
+++ src/services/index.ts
@@ -0,0 +1,7 @@
+import { StackedServices, WebSocketFactory, HttpService, Storage, RootNavController} from './services';
+
+export default StackedServices;
+export default WebSocketFactory;
+export default HttpService;
+export default Storage;
+export default RootNavController;
