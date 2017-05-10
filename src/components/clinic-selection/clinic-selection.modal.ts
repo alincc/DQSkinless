161582bc401971d0ff2a5157de86f9f752312b2b
arsy <arseniosarmiento@gmail.com>
2017-05-10 23:32:07 +0800

    bug fix on accessing null value upon subscribing to Storage Behavioral Subject

---
+++ src/components/clinic-selection/clinic-selection.modal.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { App, LoadingController, AlertController, ViewController } from 'ionic-angular';
+
+import { LoginPage } from '../../pages/login/login.page';
+import { TabsPage } from '../../pages/tabs/tabs';
+
+import { ClinicSelectionService } from './clinic-selection.service';
+import { RootNavController, Storage } from '../../services/services';
+
+@Component({
+    selector: 'clinic-selection',
+    templateUrl: 'clinic-selection.html',
+    providers: [ClinicSelectionService]
+})
+export class ClinicSelectionModal implements OnInit {
+
+    private clinics: any[];
+    private loading: any;
+
+    constructor(
+        private app: App,
+        private alertController: AlertController,
+        private loadingController: LoadingController,
+        private service: ClinicSelectionService,
+        private root: RootNavController,
+        private storage: Storage,
+        private view: ViewController) {
+    }
+
+    public ngOnInit() {
+        this.showLoading();
+        this.service.getClinicRecordByUserId().subscribe(response => {
+            if (response && response.status) {
+                this.clinics = response.result;
+            }
+            this.dismissLoading();
+        }, err => this.dismissLoading());
+    }
+
+    private showLoading() {
+        this.loading = this.loadingController.create({
+            spinner: 'crescent',
+            cssClass: 'xhr-loading'
+        });
+        this.loading.present();
+    }
+
+    private dismissLoading() {
+        if (this.loading) {
+            this.loading.dismiss();
+        }
+    }
+
+    public go(clinic) {
+        this.storage.clinic = clinic;
+        this.view.dismiss();
+        this.app.getRootNav().setRoot(TabsPage);
+    }
+
+}   
---
+++ src/components/clinic-selection/clinic-selection.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpService, Storage } from '../../services/services';
+import { CONFIG } from '../../config/config';
+
+@Injectable()
+export class ClinicSelectionService {
+
+    constructor(
+        private http: HttpService,
+        private storage: Storage) { }
+
+    private userId;
+
+    private getUserId() {
+        if (!this.userId) {
+            this.storage.accountSubject.subscribe( account => {
+                this.userId = account.userId;
+            })
+        }
+        return this.userId;
+    }
+
+
+    public getClinicRecordByUserId() {
+        return this.http.get(CONFIG.API.getClinicRecordByUserId, [this.getUserId()]);
+    }
+}
