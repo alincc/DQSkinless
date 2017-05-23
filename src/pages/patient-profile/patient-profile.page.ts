9ae62566d1304154980fc15e4be36a7d7acf8a77
papaya26 <juddisgood999@gmail.com>
2017-05-24 01:27:47 +0800

    initial pd relationship page

--- src/pages/patient-profile/patient-profile.page.ts
+++ src/pages/patient-profile/patient-profile.page.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { RootNavController } from '../../services';
 import { timeline } from './timeline.mock';
 import { ConsultationFormPage } from '../consultation-form/consultation-form';
+import { PatientDoctorRelationshipPage } from '../patient-doctor-relationship/patient-doctor-relationship.page';
 
 @Component({
-	selector:'patient-profile-page',
+	selector: 'patient-profile-page',
 	templateUrl: 'patient-profile.html'
 })
-export class PatientProfilePage{
+export class PatientProfilePage {
 	public timeline: any;
-	constructor(private nav:NavController){
+	constructor(private rootNav: RootNavController) {
 		this.timeline = timeline;
 	}
 
-	open(){
-		this.nav.push(ConsultationFormPage);
+	open() {
+		this.rootNav.push(ConsultationFormPage);
+	}
+
+	public share() {
+		// TODO pass patient id
+		this.rootNav.push(PatientDoctorRelationshipPage, {
+			pageHeader: 'Share Patient'
+		});
 	}
 }
