8b3b8b777540207d8b6f73eecb9b07025a164682
papaya26 <juddisgood999@gmail.com>
2017-05-30 01:28:03 +0800

    implemented search by filters in search user modal

--- src/components/search-user-modal/search-user-modal.component.ts
+++ src/components/search-user-modal/search-user-modal.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertController, NavParams, ViewController } from "ionic-angular";
 
 import { SearchUserModalService } from './search-user-modal.service';
 
+import { LOVS } from '../../constants/constants';
 import { Utilities } from '../../utilities/utilities';
 
 @Component({
@@ -14,10 +15,20 @@ import { Utilities } from '../../utilities/utilities';
 export class SearchUserModal implements OnInit {
 
     public searchForm: FormGroup;
+    public criteriaTypes: any;
+    public selectedCriteriaType: any;
+    public errors: any;
     public users: any;
+    public haveSearched: boolean;
 
     private message: any;
+    private pageHeader: any;
     private role: any;
+
+    public criteria: AbstractControl;
+    public username: AbstractControl;
+    public lastname: AbstractControl;
+    public firstname: AbstractControl;
 
     constructor(
         private alertController: AlertController,
@@ -35,24 +46,31 @@ export class SearchUserModal implements OnInit {
     private getDefaults() {
         this.users = [];
         this.message = this.params.data && this.params.data.message ? this.params.data.message : 'Select';
+        this.pageHeader = this.params.data && this.params.data.pageHeader ? this.params.data.pageHeader : 'Search User';
         this.role = this.params.data && this.params.data.role ? this.params.data.role : 0;
+        this.criteriaTypes = LOVS.SEARCH_BY;
+        this.selectedCriteriaType = 3;
+        this.errors = {
+            criteria: '',
+            username: '',
+            lastname: '',
+            firstname: ''
+        };
+        this.haveSearched = false;
     }
 
     private createSearchForm() {
         this.searchForm = this.formBuilder.group({
+            criteria: ['', Validators.required],
             username: '',
             lastname: '',
             firstname: ''
         });
-    }
 
-    public search(event) {
-        this.searchUserModalService.getUsers(this.searchForm.value, this.role).subscribe(response => {
-            if (response) {
-                this.users = this.removeMySelf(response);
-            }
-            event.dismissLoading()
-        });
+        this.criteria = this.searchForm.get('criteria');
+        this.username = this.searchForm.get('username');
+        this.lastname = this.searchForm.get('lastname');
+        this.firstname = this.searchForm.get('firstname');
     }
 
     private removeMySelf(users) {
@@ -61,24 +79,6 @@ export class SearchUserModal implements OnInit {
         }
 
         return users
-    }
-
-    public selectUser(user) {
-        this.alertController.create({
-            message: `${this.message} ${user.lastname ? this.getFullName(user) : user.email}?`,
-            buttons: [
-                {
-                    text: 'NO',
-                    role: 'cancel',
-                },
-                {
-                    text: 'YES',
-                    handler: () => {
-                        this.viewController.dismiss(user).catch(() => { });
-                    }
-                }
-            ]
-        }).present();
     }
 
     public getDefaultAvatar(member) {
@@ -103,5 +103,82 @@ export class SearchUserModal implements OnInit {
             return formattedUserContacts.substring(1, formattedUserContacts.length - 2);
         }
         return '';
+    }
+
+    public selectCriteria(response) {
+        this.selectedCriteriaType = response;
+        this.username.setValue('');
+        this.username.setErrors(null);
+        this.lastname.setValue('');
+        this.lastname.setErrors(null);
+        this.firstname.setValue('');
+        this.firstname.setErrors(null);
+    }
+
+    public selectUser(user) {
+        this.alertController.create({
+            message: `${this.message} ${user.lastname ? this.getFullName(user) : user.email}?`,
+            buttons: [
+                {
+                    text: 'NO',
+                    role: 'cancel',
+                },
+                {
+                    text: 'YES',
+                    handler: () => {
+                        this.viewController.dismiss(user).catch(() => { });
+                    }
+                }
+            ]
+        }).present();
+    }
+
+    private validateForm() {
+        this.errors.criteria = this.criteria.hasError('required') ? 'Search By is requried' : '';
+
+        if (this.selectedCriteriaType === 0) {
+            this.username.markAsDirty();
+
+            if (this.username.value === '') {
+                this.errors.username = 'Username is requried';
+                this.username.setErrors({ required: true });
+            } else {
+                this.username.setErrors(null);
+            }
+        } else if (this.selectedCriteriaType === 1) {
+            this.lastname.markAsDirty();
+
+            if (this.lastname.value === '') {
+                this.errors.lastname = 'Last Name is requried';
+                this.lastname.setErrors({ required: true });
+            } else {
+                this.username.setErrors(null);
+            }
+        } else if (this.selectedCriteriaType === 2) {
+            this.firstname.markAsDirty();
+
+            if (this.firstname.value === '') {
+                this.errors.firstname = 'First Name is requried';
+                this.firstname.setErrors({ required: true });
+            } else {
+                this.firstname.setErrors(null);
+            }
+        }
+    }
+
+    public search(event) {
+        this.criteria.markAsDirty();
+        this.validateForm();
+        if (this.searchForm.valid) {
+            this.haveSearched = true;
+            this.searchUserModalService.getUsers(this.searchForm.value, this.role).subscribe(response => {
+                if (response) {
+                    this.users = this.removeMySelf(response);
+                }
+                event.dismissLoading()
+            });
+        } else {
+            event.dismissLoading();
+        }
     }
 }
