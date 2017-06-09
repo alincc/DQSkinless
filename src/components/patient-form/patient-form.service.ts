a82222da40021f8167d663ab1fd8d91bb96f2345
Judo Philip F. Sosa <jpsosa@ph.indra.com>
2017-06-09 19:19:24 +0800

    rearragned patient form fields. implemented add contact to patient form

--- src/components/patient-form/patient-form.service.ts
+++ src/components/patient-form/patient-form.service.ts
@@ -3,13 +3,16 @@ import { HttpService, Storage } from '../../services';
 import { CONFIG } from '../../config/config'
 
 @Injectable()
-export class PatientService{
+export class PatientService {
     constructor(
         private http: HttpService,
         private storage: Storage) { }
 
-        public addPatientDetails(_parameter) {
+    public addPatientDetails(_parameter) {
         return this.http.post(CONFIG.API.patientDetails, _parameter);
     }
-        
+
+    public addContacts(_parameter) {
+        return this.http.post(CONFIG.API.patientContacts, _parameter);
+    }
 }
