c60a4727401d70eedd2c3073f360a2cf94ab28c3
papaya26 <juddisgood999@gmail.com>
2017-06-11 23:45:49 +0800

    removed patient relationship page and patient manager

--- src/pages/patient-profile/patient-profile.page.ts
+++ src/pages/patient-profile/patient-profile.page.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { RootNavController } from '../../services';
 import { timeline } from './timeline.mock';
 import { ConsultationFormPage } from '../consultation-form/consultation-form';
-import { PatientManagerPage } from '../patient-manager/patient-manager.page';
-import { PatientDoctorRelationshipPage } from '../patient-doctor-relationship/patient-doctor-relationship.page';
 import { PatientProfileService } from './patient-profile.service';
 import { NavParams } from "ionic-angular";
 import { Utilities } from '../../utilities/utilities';
@@ -24,12 +22,12 @@ export class PatientProfilePage {
 	private patientId;
 
 	constructor(private rootNav: RootNavController,
-				private service: PatientProfileService,
-				private navParams: NavParams) {
+		private service: PatientProfileService,
+		private navParams: NavParams) {
 		this.timeline = timeline;
 
 		this.patientId = navParams.data;
-		
+
 		this.fetchPatientInformation();
 	}
 
@@ -37,22 +35,16 @@ export class PatientProfilePage {
 		this.rootNav.push(ConsultationFormPage);
 	}
 
-	public share() {
-		this.rootNav.push(PatientManagerPage, {
-			patientId: this.patientId,
-		});
-	}
+	private fetchPatientInformation() {
 
-	private fetchPatientInformation(){
+		this.service.getPatientDetails(this.patientId).subscribe(response => {
+			if (response.status) {
 
-		this.service.getPatientDetails(this.patientId).subscribe(response =>{
-			if(response.status){
-				
 				this.address = response.result.address;
 				this.patientName = Utilities.getFullName(response.result);
 				this.address = response.result.address;
 				this.age = response.result.birthDate != null ? Utilities.getAge(response.result.birthDate) : "unknown";
-				this.registrationDate = response.result.startDate != null ? Utilities.transformDate( new Date(response.result.startDate)) : "unknown";
+				this.registrationDate = response.result.startDate != null ? Utilities.transformDate(new Date(response.result.startDate)) : "unknown";
 			}
 		})
 	}
