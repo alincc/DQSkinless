84c2b53c10f29f79c7416fb37ac780b10eca2791
arseniosarmiento@gmail.com <arseniosarmiento@gmail.com>
2017-06-23 03:12:50 +0800

    lock on patient form; reminder on dashboard

--- src/pages/patient-information/patient-information.page.ts
+++ src/pages/patient-information/patient-information.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavParams, NavController } from 'ionic-angular';
+import { NavParams, NavController, AlertController } from 'ionic-angular';
 import { RootNavController } from '../../services';
 
 @Component({
@@ -10,11 +10,13 @@ export class PatientInformationPage {
  
 
   private hasPatientId;
+  private lock: boolean;
 
   constructor( 
     private navParams: NavParams,
     private rootNav: RootNavController,
-    private nav : NavController
+    private nav : NavController,
+    private alert : AlertController
     ) {
 
         this.hasPatientId = navParams.get('patientId');
@@ -31,5 +33,54 @@ export class PatientInformationPage {
     this.nav.pop();
     this.navParams.data.addOrEditQueue(response);
 	}
+
+  toggleLock(){
+    if(this.lock){
+      return this.validatePassword().then(response => {
+        this.lock = !response;
+      })
+    }else{
+      this.lock = true;  
+    }
+  }
+
+  ionViewCanLeave(){
+    if(this.lock){
+      return this.validatePassword();
+    }else{
+      return true;
+    }
+  }
+
+  validatePassword(){
+    return new Promise((resolve, reject) => {
+         let alert = this.alert.create({
+          message: "Enter Password",
+          inputs: [{
+            name: 'password',
+            placeholder: 'Password Here',
+            type: 'password'
+          }],
+          buttons: [{
+            text: 'Cancel',
+            role: 'cancel',
+            handler: data => {
+              resolve(false);
+            }
+          },{
+            text: 'Proceed',
+            handler: data => {
+              if(data.password === 'admin'){
+                resolve(true);
+              }else{
+                return false;
+              }
+            }
+          }
+          ]
+        });
+        alert.present();
+      })
+  }
 
 }
